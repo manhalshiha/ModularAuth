f1ca925cd53ee9e1a4c363bcdf54abba1a42b1cd
manhal shiha <manhal6@gmail.com>
2026-02-20 13:49:06 +0300

    Initial commit

---
+++ ModularAuth.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace ModularAuth.Domain.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ ModularAuth.Domain/Common/Results/Error.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModularAuth.Domain.Common.Results
+{
+    internal class Error
+    {
+    }
+}
